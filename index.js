67d9a42242b98cf47848e35e68180c4cdc58ba1f
Jesús Leganés Combarro 'piranna <piranna@gmail.com>
2016-05-10 22:17:22 +0200

    Code extracted from ContextBroker & added support for Date objects

---
+++ index.js
@@ -0,0 +1,78 @@
+var duration = require('moment').duration
+
+
+/* https://github.com/moment/moment/blob/develop/src/lib/duration/create.js */
+const isoRegex = /^(-)?P(?:(?:([0-9,.]*)Y)?(?:([0-9,.]*)M)?(?:([0-9,.]*)D)?(?:T(?:([0-9,.]*)H)?(?:([0-9,.]*)M)?(?:([0-9,.]*)S)?)?|([0-9,.]*)W)$/
+
+
+// Add a JSON representation for RegExp objects
+if(RegExp.prototype.toJSON === undefined)
+   RegExp.prototype.toJSON = RegExp.prototype.toString
+
+
+/**
+ * Check if an object is a number or represent one of them
+ *
+ * @param {*} value
+ *
+ * @return {Boolean}
+ */
+function isNumber(value)
+{
+  return !isNaN(value)
+}
+
+/**
+ * Create Javascript objects and primitives from their string representation
+ *
+ * Promote the string fields to Javascript objects when the server returns a
+ * JSON with only strings on its values. The current promotions are {Boolean},
+ * {Number}, {RegExp} {duration} and {Date}.
+ *
+ * @param {*} value
+ *  value to be converted in a Javascript object
+ */
+function string2js(value)
+{
+  if(typeof value === 'string')
+  {
+    // Boolean
+    if(value === 'false') return false
+    if(value === 'true')  return true
+
+    // Number
+    if(isNumber(value)) return parseFloat(value)
+
+    // RegExp
+    var length = value.length
+    if(length > 1
+    && '/' === value[0]
+    && '/' === value[length-1])
+      return RegExp(value)
+
+    // ISO8601 duration
+    if(value.match(isoRegex)) return duration(value)
+
+    // Date
+    var date = new Date(value)
+    if(date.toString() !== 'Invalid Date') return date
+  }
+
+  return value
+}
+
+/**
+ * Create Javascript objects from the string fields on a JSON object
+ *
+ * @param {string} key
+ *  key of the JSON object, ignored
+ * @param {*} value
+ *  value to be converted in a Javascript object
+ */
+string2js.reviver = function(key, value)
+{
+  return string2js(value)
+}
+
+
+module.exports = string2js
